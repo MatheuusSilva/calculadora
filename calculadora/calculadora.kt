acdb7cd52d75626dbe3bc8707c8914511491ced3
MatheuusSilva <matheusalves.spk@gmail.com>
2025-04-05 01:05:27 -0300

    Update calculadora.kt

--- calculadora/calculadora.kt
+++ calculadora/calculadora.kt
@@ -14,35 +14,35 @@ fun main() {
         
     when (opcao) {
         1 -> {
-            print("Digite o valor para depósito: ")
+            print("Digite o valor para depósitar: ")
             val valor = readLine()?.toDoubleOrNull() ?: 0.0
             if (valor > 0) {
                 saldo += valor
-                println("Depósito de R$${"%.2f".format(valor)} realizado com sucesso!")
+                println("Depósito de R$${"%.2f".format(valor)} feito com sucesso")
             } else {
-        println("Valor inválido para depósito!")
+        println("Valor inválido para depósito")
             }
             }
         2 -> {
-            print("Digite o valor para saque: ")
+            print("Digite o valor para sacar: ")
             val valor = readLine()?.toDoubleOrNull() ?: 0.0
         if (valor > 0 && valor <= saldo) {
                  saldo -= valor
-                println("Saque de R$${"%.2f".format(valor)} realizado com sucesso!")
+                println("Saque de R$${"%.2f".format(valor)} feito com sucesso")
           } else if (valor > saldo) {
                 println("Saldo insuficiente!")
           } else {
-                println("Valor inválido para saque!")
+                println("Valor inválido de saque")
             }
-            }
+           }
         3 -> {
             println("Seu saldo atual é: R$${"%.2f".format(saldo)}")
             }
         4 -> {
-           println("Obrigado por usar nosso caixa eletrônico!")
+           println("Serviço encerrado.")
             }
          else -> {
-         println("Opção inválida! Tente novamente.")
+         println("Opção inválida, Tente novamente.")
           }
         }
     } while (opcao != 4)
